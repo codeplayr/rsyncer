cf089782d737622911780e1a9fd5c6107a8c5e68
codeplayr <nufan2k@gmx.de>
2016-11-16 02:33:47 +0100

    fixed flag validation

--- src/Helper.php
+++ src/Helper.php
@@ -4,23 +4,40 @@ namespace Codeplayr\Rsyncer;
 class Helper{
 	
 	public function removeDash( $flag ){
-		if( ! is_string( $flag ) ){
+		if( ! is_string( $flag ) || empty($flag)  ){
 			throw new \InvalidArgumentException('No String Value');
 		}
 		
-		if( strlen( $flag ) > 2 && $flag[0] == '-' ){
-			$left = substr($flag, 0, 2);
-			$right = substr($flag, 2);
-			$left = str_replace('-', '' , $left);	
-			$flag = $left . $right;
+		if( $flag[0] == '-'){
+			
+			if( strlen( $flag ) > 2 ){
+				$left = substr($flag, 0, 2);
+				$right = substr($flag, 2);
+				$left = str_replace('-', '' , $left);	
+				$flag = $left . $right;
+			}
+			else if( strlen( $flag ) == 2 ){
+				$flag = substr($flag, 1);
+			}
+			else{
+				throw new \InvalidArgumentException('Invalid Flag Value');
+			}
+		
 		}
+		
+		if( empty( $flag ) || $flag[0] == '-' ){
+			throw new \InvalidArgumentException('Invalid Assembled Flag');
+		}
+		
 		return $flag;
 	}
 	
 	public function addDash( $flag ){
-		if( ! is_string( $flag ) ){
+		if( ! is_string( $flag ) || empty($flag) ){
 			throw new \InvalidArgumentException('No String Value');
 		}
+		
+		$flag = $this->removeDash( $flag );
 		
 		return (strlen($flag) > 1) ? '--' . $flag : '-' . $flag;
 	}
